c6cf022344954058418f4a78e437a00fd5119b7c
Shree3245 <Shreevenkr@gmail.com>
2020-09-19 10:48:59 +0530

    final optimization

---
+++ dScan.py
@@ -0,0 +1,128 @@
+import requests
+import re
+import sys
+import time
+import os
+import argparse
+from bs4 import BeautifulSoup
+from functools import partial
+from multiprocessing import Pool, TimeoutError, cpu_count
+from fake_useragent import UserAgent
+
+ua = UserAgent().random
+
+parser = argparse.ArgumentParser(
+	description='Argument parser for dork-scanner')
+parser.add_argument(
+	'-S', '--search', help='String to be searched for', default='1')
+parser.add_argument(
+	'-E', '--engine', help='Search engine to be used', default='google')
+parser.add_argument(
+	'-P', '--page', help='Number of pages to search in', default='1')
+parser.add_argument('-Pr', '--process',
+					help='Number of parallel processes', default='1')
+results = parser.parse_args(sys.argv[1:])
+
+engineDict = {
+	'google': {
+		'link': 'http://www.google.com/search',
+		'tags': {
+			'options': 'cite',
+			'attr': {
+				'class': 'iUh30'
+			}
+		}
+	},
+
+	'bing': {
+		'link': 'https://www.bing.com/search',
+		'tags': {
+			'options': 'li',
+			'attr': 'b_algo'
+		}
+	},
+
+	'baidu': {
+		'payload': {
+			'q': 'string',
+			'first': 'start'
+		},
+		'link': 'http://www.baidu.com/s',
+		'tags': {
+			'options': 'h3',
+			'attr': 't'
+		}
+	},
+}
+
+
+def search(engine, string, start):
+	engineSearch = engineDict[engine]
+	urls = []
+	payload = { 'q' : string, 'start' : start }
+	headers = {'User-agent': ua}
+	req = requests.get(engineSearch['link'],
+					   payload, headers=headers)
+	soup = BeautifulSoup(req.text, 'html.parser')
+	tags = soup.findAll(engineSearch['tags']
+						['options'], engineSearch['tags']['attr'])
+
+	for tag in tags:
+		try:
+			if engine == 'google':
+				urls.append(tag.text)
+			elif engine == 'bing':
+				urls.append(tag.find('a').attrs['href'])
+			else:
+				urlu = tag.find('a').attrs['href']
+				link = requests.get(urlu)
+				urls.append(link.url)
+		except:
+			continue
+	return urls
+
+def printf(lista): 
+	for i in lista:
+		link = str(i)
+		ch = link.replace("%3F", "?")
+		ch2 = ch.replace("%3D","=")
+		print( " " + ch2 )
+
+
+def main():
+	try:
+		print(results)
+		string = results.search
+		engine = results.engine
+		page   = results.page
+		proc    = int( results.process  )
+	except:
+		print(" * * Error * * Arguments missing")
+		exit(0)
+	start_time = time.time()
+	result = []
+	pages = []
+
+	for p in range(int(page)):
+		pages.append(p*10)
+		p = Pool(proc)
+		print("#"*50)
+		print ("Searching for: {} in {} page(s) of {} with {} process(es)".format(str(string),str(page),str(engine),str(proc)))
+		print ("#"*50)
+		print ("\n")
+		request = partial(search,engine,string)
+		listAll = p.map(request,pages)
+
+	for p in listAll:
+		result += [u for u in p]
+		printf(set(result))
+
+	print ("\n")
+	print ("#"*50)
+	print( " Number of urls : {}" . format( str( len( result ) ) ))
+	print( " Finished in : {} s" . format( str( int( time.time() - start_time ) )))
+	print ("#"*50)
+
+if __name__ == '__main__':
+	main()
+
